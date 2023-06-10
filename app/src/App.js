710f4fd21fc86110289db01a8fc05848ca23ee96
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-06-10 09:17:08 +0100

    add router

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 // import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './component/navbar/navbar.jsx'
 import Home from './component/home/home';
 import About from './component/about/about';
@@ -12,16 +13,20 @@ function App() {
   return (
 
       <div class="app">
+        <BrowserRouter>
             <Navbar />
-            <Router>
-              <Routes>
-                <Route path="/" element={<Home  />} />
-                <Route path="/Potfolio-Website/" element={<Home  />} />
-                <Route path="/Potfolio-Website/About/" element={<About  />} />
-                <Route path="/Project/" element={<Project  />} />
-                <Route path="/Contact/" element={<Contact  />} />
-              </Routes>
-            </Router>
+            {/* <Router> */}
+                <Routes>
+                    <Route path="/" element={<Home  />} />
+                    <Route path="/Potfolio-Website/" element={<Home  />} />
+                    <Route path="/Potfolio-Website/About/" element={<About  />} />
+                    <Route path="/Project/" element={<Project  />} />
+                    <Route path="/Contact/" element={<Contact  />} />
+                </Routes>
+            {/* </Router> */}
+        </BrowserRouter>
+            
+            
       </div>
 
   );
