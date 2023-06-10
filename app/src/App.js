7b16697f15a6da95bef33f4bfa10f4e66ad6f7b2
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-06-10 09:49:43 +0100

    add router

--- app/src/App.js
+++ app/src/App.js
@@ -1,21 +1,28 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './component/navbar/navbar.jsx'
 import Home from './component/home/home';
 import About from './component/about/about';
 import Project from './component/project/project';
 import Contact from './component/contact/contact';
-
-
 function App() {
   return (
 
       <div class="app">
-        <BrowserRouter>
+
             <Navbar />
-            {/* <Router> */}
+            <Router basename='/Potfolio-Website'>
+              <Routes>
+                    <Route path="/" element={<Home  />} />
+                    <Route path="/Potfolio-Website/" element={<Home  />} />
+                    <Route path="/Potfolio-Website/About/" element={<About  />} />
+                    <Route path="/Project/" element={<Project  />} />
+                    <Route path="/Contact/" element={<Contact  />} />
+              </Routes>
+            </Router>
+
+
+            {/* <Router >
                 <Routes>
                     <Route path="/" element={<Home  />} />
                     <Route path="/Potfolio-Website/" element={<Home  />} />
@@ -23,8 +30,7 @@ function App() {
                     <Route path="/Project/" element={<Project  />} />
                     <Route path="/Contact/" element={<Contact  />} />
                 </Routes>
-            {/* </Router> */}
-        </BrowserRouter>
+            <Router /> */}
             
             
       </div>
