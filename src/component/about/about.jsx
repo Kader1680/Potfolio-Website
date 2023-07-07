d13f40e8c12eb14e71216e7e4e1507e44758b712
Kader1680 <ouldhenniabaghdad@gmail.com>
2023-07-07 23:25:58 +0200

    add

--- src/component/about/about.jsx
+++ src/component/about/about.jsx
@@ -2,6 +2,21 @@ import React from 'react'
 import img from "../../assests/img.jpg"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './about.css'
+import html from "../../assests/html.png"
+import cc3 from "../../assests/css3.png"
+import heroku from "../../assests/heroku.png"
+import firebase from "../../assests/firebase.png"
+import git from "../../assests/git.png"
+import github from "../../assests/github.png"
+import c from "../../assests/c.png"
+import jquery from "../../assests/jquery.png"
+import sass from "../../assests/sass.png"
+import Tailwind from "../../assests/Tailwind.png"
+import php from "../../assests/php.png"
+import javascript from "../../assests/javascript.png"
+import mysql from "../../assests/mysql.png"
+import amaz from "../../assests/mysql.png"
+import bootstrap from "../../assests/bootstrap.png"
 export default function About() {
       return (
       <div class="about row">
@@ -13,22 +28,82 @@ export default function About() {
             <div className='right col-xs-12 col-sm-12 col-md-6 col-lg-6 col-xl-6'>
                   
                   <div class="DataPersonel">
-                  <h3>Name:</h3> <span>Abdelkader Ould Hennia</span>
-                  <h3>Birth Date</h3> <span>23 January 2000</span>
-                  <h3>Phone Number: </h3> <span>+213549891600</span>
-                  <h3>Gmail:</h3> <span>oudlhenniabaghdad@gmail.com</span>
-                  <h3>Address:</h3> <span>Mazouna Rlizane Algeria</span>
+                        <h3>I'm  <span>Abdelkader Ould Hennia</span></h3>
+                        <h3>Full Stack Developer</h3>
+                        <p>I am a Full-Stack developer based in Pune, India. I am an Information Technology undergraduate from SPPU. I am very passionate about improving my coding skills & developing applications & websites. I build WebApps and Websites using MERN Stack. Working for myself to improve my skills. Love to build Full-Stack clones.</p>
+                        <p>Email : jigarsable21@gmail.com</p>
+                        <p>Place : Pune, India - 412206</p>
+                  </div>
+
+            </div>
+            <h4 className=' text-center'> Skills & Abilities</h4>
+            <div className='skill text-center'>
+
+                  <div>
+                        <img src= {html} />
+                  </div>
+
+                  <div>
+                       <img src= {cc3} />
+
+                  </div>
+                  <div>
+                       <img src= {php} />
+
+                  </div>
+                  <div>
+                       <img src= {c} />
+
+                  </div>
+                  <div>
+                       <img src= {sass} />
+
+                  </div>
+                  <div>
+                       <img src= {bootstrap} />
+
+                  </div>
+                  <div>
+                       <img src= {mysql} />
+
+                  </div>
+                  <div>
+                       <img src= {bootstrap} />
+
+                  </div>
+                  <div>
+                       <img src= {jquery} />
+
+                  </div>
+
+                  <div>
+                       <img src= {heroku} />
+
+                  </div>
+
+                  <div>
+                       <img src= {git} />
+
+                  </div>
+
+                  <div>
+                       <img src= {github} />
+
+                  </div>
+                  <div>
+                       <img src= {amaz} />
+
+                  </div>
+                  <div>
+                       <img src= {firebase} />
+
+                  </div> 
+                  <div>
+                       <img src= {bootstrap} />
                   </div>
             </div>
 
-            <div class="description">
-                  <h3>Who am I </h3>
-                  <p>Front End React Developer 2-year experienced who enjoys building everything with JavaScript.
-
-                        In this duration, I have learned a lot of technologies, created many cool websites, collaborated with great developers and contributed to open source projects.
-
-                        So I'm always seeking to have an opportunity that matches my skills, to make the best use of all what I have learned.</p>
-            </div>
+            
       </div>
       )
 }
--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
-import own  from "../../assests/own.png"
+import me  from "../../assests/me.png"
 import  "./home.css"
 export default function Home() {
   return (
     <div  class="row home">
-        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
-            <img src={own} />
-        </div>
+        
         <div className='col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
             <h1>Hi There,I'm Abdelkader <span>Ould Hennia</span></h1>
            
@@ -14,13 +12,16 @@ export default function Home() {
                 Resume
                 <i class="ms-2 fa-solid fa-download"></i>
             </a>
-            <div className='social'>
+            <div className='social '>
                   <a href="https://www.linkedin.com/in/abdelkader-ould-hennia/" target='_blank'><i class="fa-brands fa-linkedin"></i></a>
                   <a href="https://www.facebook.com/baghdad.ouldhennia.9" target='_blank'><i class="fa-brands fa-facebook"></i></a>
                   <a href='https://github.com/Kader1680' target='_blank'><i class="fa-brands fa-github"></i></a>
                   <a href='https://github.com/Kader1680' target='_blank'><i class="fa-brands fa-twitter"></i></a>
                   <a href='https://github.com/Kader1680' target='_blank'><i class="fa-brands fa-dev"></i></a>
             </div>
+        </div>
+        <div className='col-xs-12 col-sm-12 col-md-12 col-lg-6 col-xl-6'>
+            <img src={me} />
         </div>
     </div>
   )
